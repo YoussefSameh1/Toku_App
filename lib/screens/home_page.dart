a59f7874a1898425ce4da6b90b655159efa895b8
Youssef <ys1465@fayoum.edu.eg>
2025-06-07 12:26:43 +0300

    toku app

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:toku/screens/colors_page.dart';
+import 'package:toku/screens/family_members.dart';
+import 'package:toku/screens/numbers_page.dart';
+import 'package:toku/screens/phrases_page.dart';
+import '../components/category_item.dart';
+
+class HomePage extends StatelessWidget{
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Toku', style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.brown,
+      ),
+      body: Column(
+        children: [
+          Category(
+            text: 'Numbers',
+            color:  Colors.orange,
+            onTap: () {
+              Navigator.push(context,
+              MaterialPageRoute(builder: (context){
+                return const NumbersPage();
+              }));
+            },
+          ),
+          Category(
+            text: 'Family Members', 
+            color: Colors.green,
+            onTap: () {
+              Navigator.push(context,
+              MaterialPageRoute(builder: (context){
+                return const FamilyMembersPage();
+              }));
+            },),
+          Category(
+            text: 'Colors',
+            color: Colors.purple,
+            onTap: () {
+              Navigator.push(context,
+              MaterialPageRoute(builder: (context){
+                return const ColorsPage();
+              }));
+            },),
+          Category(
+            text: 'Phrases',
+            color: Colors.blue,
+            onTap: () {
+              Navigator.push(context,
+              MaterialPageRoute(builder: (context){
+                return const PhrasesPage();
+              }));
+            },)
+        ],
+      ),
+    );
+  }
+}
